63ece0454ad91c527b4b4bdee670b4e9805165e0
kajsamp <kajsamp@met.no>
2017-04-20 12:28:59 +0000

    updated cds, added as.field.commonEOFS
    Former-commit-id: dfef761a86dae004a29539e08f831bac5eec1a04

--- back-end/R/cds.R
+++ back-end/R/cds.R
@@ -30,8 +30,8 @@ getCM <- function(url=NULL,destfile='CM.nc',lon=NULL,lat=NULL,force=FALSE,verbos
   cid$area.sd <- aggregate.area(X,FUN='sd')
   cid$url <- url
   cid$dates <- paste(range(index(X)),collapse=",")
-  ncid <- nc_open(destfile)
   ## Collect information stored as model attributes
+  ncid <- nc_open(destfile)
   model <- ncatt_get(ncid,0)
   nc_close(ncid)
   cid$model <- model
@@ -48,8 +48,12 @@ getGCMs <- function(select=1:9,varid='tas',destfile=NULL,verbose=FALSE) {
   url <- cmip5.urls(varid=varid)[select] ## Get the URLs of the 
   ## Set up a list variable to contain all the metadata in sub-lists.
   X <- list()
-  for (i in seq_along(select)) X[[paste('gcm',select[i],sep='.')]] <- getCM(url=url[i],destfile=destfile[i])
-  return(X)
+  for (i in seq_along(select)) {
+    if(verbose) print(paste("Get gcm.",select[i],sep=''))
+    X[[paste('gcm',select[i],sep='.')]] <-
+      getCM(url=url[i],destfile=destfile[i])
+  }
+  invisible(X)
 }
 
 testGCM <- function(select=1:9,varid='tas',path=NULL,verbose=FALSE) {
@@ -70,7 +74,6 @@ testGCM <- function(select=1:9,varid='tas',path=NULL,verbose=FALSE) {
   return(X)
 }
 
-
 ## Specific model to retrieve RCMs
 getRCMs <- function(select=1:9,varid='tas',destfile=NULL,verbose=FALSE) {
   if(verbose) print("getRCMs")
@@ -80,8 +83,12 @@ getRCMs <- function(select=1:9,varid='tas',destfile=NULL,verbose=FALSE) {
   url <- cordex.urls()[select]
   ## Set up a list variable to contain all the metadata
   X <- list()
-  for (i in seq_along(select)) X[[paste('rcm',select[i],sep='.')]] <- getCM(url=url[i],destfile=destfile[i])
-  return(X)
+  for (i in seq_along(select)) {
+    if(verbose) print(paste("Get rcm.",select[i],sep=""))
+    X[[paste('rcm',select[i],sep='.')]] <-
+      getCM(url=url[i],destfile=destfile[i])
+  }
+  invisible(X)
 }
 
 ## Compute the common EOFs for GCMs and save the results for the front-end
@@ -92,33 +99,49 @@ commonEOFS.gcm <- function(select=1:9,varid='tas',destfile=NULL,
   getGCMs(select=select,varid=varid,destfile=destfile)
   X <- NULL
   for (fname in destfile) {
-    x <- retrieve(fname)
+    if(verbose) print(paste("retrieve",fname))
+    x <- retrieve(fname,verbose=verbose)
     if (!is.null(it)) {
-      if (tolower(it)=='annual') x <- annual(x) else
-                                 x <- subset(x,it=it,is=is)
+      if (tolower(it)=='annual') x <- annual(x,verbose=verbose) else
+                                 x <- subset(x,it=it,is=is,verbose=verbose)
     }
-    if (is.null(X)) X <- x else X <- combine(X,x)
+    if (is.null(X)) X <- x else X <- combine(X,x,verbose=verbose)
   }
-  ceof <- EOF(X)
+  if(verbose) print("Calculate common EOF")
+  ceof <- EOF(X,verbose=verbose)
   plot(ceof)
+  
   ## Need to reformat the ceof-object to fit the set-up for the R-shiny app in the front-end.
+  if(verbose) print("Reformat the common EOF object")
   x1 <- coredata(ceof); attributes(x1) <- NULL; dim(x1) <- dim(ceof)
-  Z <- list(info='CORDEX runs',eof=ceof,rcm.1=zoo(x1,order.by=index(ceof)))
+  Z <- list(info='CMIP5 runs',eof=as.eof(ceof),rcm.1=zoo(x1,order.by=index(ceof)))
   clim <- list(rcm.1=map.field(X,plot=FALSE))
-  rcmnames <- attr(ceof,'model_id')
+  gcmnames <- attr(ceof,'model_id')
   for (i in 1:attr(ceof,'n.apps')) {
-    x1 <- coredata(attr(ceof,paste('appendix.',i,sep='')))
-    #attributes(x1) <- NULL; dim(x1) <- dim(attr(ceof,paste('appendix.',i,sep='')))
-    Z[[paste('rcm.',i+1,sep='')]] <- zoo(x1,order.by=index(paste('appendix.',i,sep='')))
-    rcmnames <- c(rcmnames,attr(x1,'model_id'))
-    #paste('appendix.',i,sep='') <- NULL
+    x1 <- attr(ceof,paste('appendix.',i,sep=''))
+    Z[[paste('rcm.',i+1,sep='')]] <- zoo(coredata(x1),order.by=index(x1))
+    gcmnames <- c(gcmnames,attr(x1,'model_id'))
     clim[[paste('rcm.',i+1,sep='')]] <- map.field(attr(X,paste('appendix.',i,sep='')))
   }
   attr(Z,'mean') <- clim
-  attr(Z,'model_id') <- rcmnames
+  attr(Z,'model_id') <- gcmnames
   class(Z) <- c('dsensemble','eof','list')
   ceof <- Z
   save(ceof,file=paste('ceof.gcm',varid,it,'rda',sep='.'))
+  invisible(ceof)
+}
+
+as.field.commonEOFS <- function(x,verbose=FALSE) {
+  if(verbose) print("as.field.ceof")
+  Y <- as.field(x, anomaly=TRUE, verbose=verbose)
+  Y0 <- Y
+  for(i in 1:length(Y)) {
+    clim.i <- coredata(attr(x,"mean")[[i]])
+    Y.i <- coredata(Y[[i]]) 
+    Y.i <- aperm(apply(Y.i,1,function(x) x + clim.i), c(2,1))
+    coredata(Y[[i]]) <- Y.i
+  }
+  invisible(Y)
 }
 
 ## Compute the common EOFs for RCMs save the results for the front-end
@@ -126,39 +149,46 @@ commonEOFS.rcm <- function(select=1:9,varid='tas',destfile=NULL,
                            it='annual',is=NULL,verbose=FALSE) {
   if(verbose) print("commonEOFS.rcm")
   if(is.null(destfile)) destfile <- paste(rep('CM',length(select)),select,'.nc',sep='')
-  getRCMs(select=select,varid=varid,destfile=destfile)
+  getRCMs(select=select,varid=varid,destfile=destfile,verbose=verbose)
   X <- NULL
   for (fname in destfile) {
+    if(verbose) print(paste("retrieve",fname))
     x <- retrieve(fname)
     if (!is.null(it)) {
-      if (tolower(it)=='annual') x <- annual(x) else
-        x <- subset(x,it=it,is=is)
+      if (tolower(it)=='annual') x <- annual(x,verbose=verbose) else
+        x <- subset(x,it=it,is=is,verbose=verbose)
     }
-    if (is.null(X)) X <- x else X <- combine(X,x)
-    
+    if (is.null(X)) X <- x else X <- combine(X,x,verbose=verbose)
   }
-  ceof <- EOF(X)
+  if(verbose) print("Calculate common EOF")
+  ceof <- EOF(X,verbose=verbose)
   plot(ceof)
   
   ## Need to reformat the ceof-object to fit the set-up for the R-shiny app in the front-end.
+  if(verbose) print("Reformat the common EOF object")
   x1 <- coredata(ceof); attributes(x1) <- NULL; dim(x1) <- dim(ceof)
-  Z <- list(info='CORDEX runs',eof=ceof,rcm.1=zoo(x1,order.by=index(ceof)))
+  Z <- list(info='CORDEX runs',eof=as.eof(ceof),rcm.1=zoo(x1,order.by=index(ceof)))
   clim <- list(rcm.1=map.field(X,plot=FALSE))
   rcmnames <- attr(ceof,'model_id')
+  gcmnames <- attr(ceof,'driving_model_id')
+  gcmrip <- attr(ceof,'driving_model_ensemble_member')
   for (i in 1:attr(ceof,'n.apps')) {
     xi <- attr(ceof,paste('appendix.',i,sep=''))
     rcmnames <- c(rcmnames,attr(xi,'model_id'))
+    gcmnames <- c(gcmnames,attr(xi,'driving_model_id'))
+    gcmrip <- c(gcmrip,attr(xi,'driving_model_ensemble_member'))
     Z[[paste('rcm.',i+1,sep='')]] <- zoo(coredata(xi),order.by=index(xi))
     clim[[paste('rcm.',i+1,sep='')]] <- map.field(attr(X,paste('appendix.',i,sep="")))
   }
   attr(Z,'mean') <- clim
   attr(Z,'model_id') <- rcmnames
+  attr(Z,'driving_model_id') <- gcmnames
+  attr(Z,'driving_model_ensemble_members') <- gcmrip
   class(Z) <- c('dsensemble','eof','list')
   ceof <- Z
-  ##browser()
   save(ceof,file=paste('ceof.rcm',varid,it,'rda',sep='.'))
+  invisible(ceof)
 }
-
 
 cmip5.urls <- function(experiment='rcp45',varid='tas',
                        url="http://climexp.knmi.nl/CMIP5/monthly/",#path=NULL,
@@ -428,4 +458,3 @@ metaextract.cordex <- function(x=NULL, verbose=FALSE) {
   }
   return(X)
 }
-
