385788b65ad7d7ee00bf38a64d6f06c6a8d698c8
kajsamp <kajsamp@met.no>
2017-05-22 09:34:37 +0200

    testing some front-end ideas in test.frontend
    Former-commit-id: 810a37b18560329b4fbcdfadcb8afd3865948ae5

--- front-end/test.frontend.R
+++ front-end/test.frontend.R
@@ -14,50 +14,66 @@ ceof.pr.gcms <- ceof
 tas.gcms <- as.field.commonEOFS(ceof.tas.gcms)
 pr.gcms <- as.field.commonEOFS(ceof.pr.gcms)
 
-dt <- function(x,it1=c(1981,2010),it2=c(2071,2100),FUN="mean") {
-  x1 <- mean(aggregate.area(subset(x,it=it1),FUN=FUN))
-  x2 <- mean(aggregate.area(subset(x,it=it2),FUN=FUN))
-  return(x2-x1)
-}
+dtdprplot <- function(tas,pr=NULL,im=c(2),pal="cat",lplot=TRUE,new=TRUE) {
 
-dtdprplot <- function(tas,pr,is=c(1,2),pal="cat",new=TRUE) {
-  if(is.null(dim(tas[[1]]))) {
-    dtas <- tas
+  dt <- function(x,it1=c(1981,2010),it2=c(2071,2100),is=NULL,FUN="mean") {
+    if(!is.null(is)) x <- subset(x,is=is)
+    x1 <- mean(aggregate.area(subset(x,it=it1),FUN=FUN))
+    x2 <- mean(aggregate.area(subset(x,it=it2),FUN=FUN))
+    return(x2-x1)
+  }
+
+  if(is.null(pr) & length(tas)==2) {
+    dtas <- tas[[1]]
+    dpr <- tas[[2]]
   } else {
-    dtas <- lapply(tas,dt)
-  } 
-  if(is.null(dim(tas[[1]]))) {
-    dpr <- pr
-  } else {
-    dpr <- lapply(pr,dt)  
-  } 
-  xlim <- c(-3,3)
-  ylim <- c(-0.2,0.2)
-  colscal(n=length(dtas),col=pal)
-  pch <- 1:length(dtas)
-  cex <- rep(1.5,length(dtas))
-  cex[is] <- 2.5
-  lwd <- rep(1.5,length(dtas))
-  lwd[is] <- 2.5
-  ## Add rcp label  
-  if(new) dev.new()
-  plot(unlist(dtas),unlist(dpr),col=col,pch=pch,cex=cex,lwd=lwd,
-       xlim=xlim,ylim=ylim,
-       xlab="Temperature change (degC)",
-       ylab="Precipitation change (mm/day)",
-       main="Global mean climate change\n(1981-2010 to 2071-2100)")
-  lines(xlim*1.5,rep(0,2),lwd=0.2)
-  lines(rep(0,2),ylim*1.5,lwd=0.2)
-  grid()
-  legend("bottomleft",ncol=2,col=col,pch=pch,cex=cex*0.5,
-         lwd=lwd*0.5,lty="none",
-         legend=paste(attr(tas,"model_id")$gcm,
-                      attr(tas,"model_id")$gcm_rip,sep="."))
+    if(is.null(dim(tas[[1]]))) {
+      dtas <- tas
+    } else {
+      dtas <- lapply(tas,dt)
+    }
+    if(is.null(dim(tas[[1]]))) {
+      dpr <- pr
+    } else {
+      dpr <- lapply(pr,dt)  
+    }
+  }
+  
+  if(lplot) {
+    xlim <- c(-3,3)
+    ylim <- c(-0.2,0.2)
+    if(!is.null(pal)) {
+      col <- colscal(n=length(dtas),col=pal)
+    } else {
+      col <- rep("grey50",length(dtas))
+    }
+    col[im] <- "black"
+    pch <- 1:length(dtas)
+    cex <- rep(1,length(dtas))
+    cex[im] <- 2.5
+    lwd <- rep(1.5,length(dtas))
+    lwd[im] <- 2.5
+    ## Add rcp label  
+    if(new) dev.new()
+    plot(unlist(dtas),unlist(dpr),col=col,pch=pch,cex=cex,lwd=lwd,
+         xlim=xlim,ylim=ylim,
+         xlab="Temperature change (degC)",
+         ylab="Precipitation change (mm/day)",
+         main="Global mean climate change\n(1981-2010 to 2071-2100)")
+    lines(xlim*1.5,rep(0,2),lwd=0.2)
+    lines(rep(0,2),ylim*1.5,lwd=0.2)
+    grid()
+    legend("bottomleft",ncol=2,pch=pch,cex=0.8,col=col,
+           bg=adjustcolor("white",alpha=0.6),box.lwd=0.5,
+           legend=paste(attr(tas,"model_id")$gcm,
+                        attr(tas,"model_id")$gcm_rip,sep="."))
+  }
+
   dtdpr <- list(dtas=dtas,dpr=dpr)
   attr(dtdpr,"model_id") <- attr(tas,"model_id")
   return(dtdpr)
-} 
+}
 
 dtdpr <- dtdprplot(tas.gcms,pr.gcms)
-dtdprplot(dtdpr$dtas,dtdpr$dpr)
+dtdprplot(dtdpr,pal="rainbow")
 
