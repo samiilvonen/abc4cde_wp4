1b56b430eb0bc6eef9772255a0747f0eec242093
Olle Raty <olle.raty@helsinki.fi>
2017-05-24 12:47:51 +0000

    fixed small typo
    Former-commit-id: 50efbabbd574e915ecbe9926412a017875fbb7a0

--- R-scripts/statistics.cmip.R
+++ R-scripts/statistics.cmip.R
@@ -1,5 +1,9 @@
 #!/usr/bin/env Rscript
 
+#Script that calculates statistics for a given period and 
+#
+#Olle Räty
+
 #Set environmental variables
 Sys.setenv(EXTERNAL_DATA = "/home/ubuntu/Data")
 Sys.setenv(PROTOTYPE_SCRIPT = "/home/ubuntu/abc4cde_wp4/R-scripts")
@@ -25,9 +29,9 @@ option_list <- list(
               dest = "verbose", help = "Suppress run-time information"),
   make_option(c("-r", "--reference"), action = "store", default = "era",
               help = "Reference data set[default %default]"),
-  make_option(c("-s", "--period1"), action = "store",default = 1981,
+  make_option(c("-s", "--period1"), action = "store",default = 2071,
               help = "reference period start [default %default]"),
-  make_option(c("-e", "--period2"), action = "store",default = 2010,
+  make_option(c("-e", "--period2"), action = "store",default = 2100,
               help = "reference period end [default %default]"),
   make_option(c("-a", "--variable"), action = "store",default = "tas",
               help = "variable [default %default]"),
@@ -45,21 +49,27 @@ opt <- parse_args(OptionParser(option_list = option_list))
 calculate.statistics.cmip <- function(reference="era", period=c(1981,2010), variable="tas", nfiles=5,
                                   continue=T, verbose=F, mask="coords.txt"){
   
+  if(is.null(reference)){
+    store.file <- paste("statistics.cmip", reference, variable, paste(period, collapse="-"), "Rdata", sep=".") 
+  } else{
+    store.file <- paste("statistics.cmip", variable, paste(period, collapse="-"), "Rdata", sep=".")
+  }
   shape <- get.shapefile("referenceRegions.shp")
   srex.regions <- as.character(shape$LAB)
-  store.file <- paste("statistics.cmip", reference, variable, paste(period, collapse="-"), "Rdata", sep=".")
   store <- list()
   if(file.exists(store.file))store <- readRDS(store.file)
 
-  ref.file <- getReference(reference,variable)
-  store.name <- paste(reference,variable,sep=".")
-  store[[store.name]]$spatial.sd <- c(cdo.spatSd(ref.file,period), cdo.spatSd(ref.file,period,seasonal=T))
-  store[[store.name]]$mean <- c(cdo.mean(ref.file,period), cdo.mean(ref.file,period,seasonal=T))
-  
-  for(i in 1:length(srex.regions)){
-    getPolCoords(shape,i,destfile=mask)
-    store[[ store.name ]][[ srex.regions[i] ]]$spatial.sd <- c(cdo.spatSd(ref.file,period,mask=mask), cdo.spatSd(ref.file,period,mask=mask,seasonal=T))
-    store[[ store.name ]][[ srex.regions[i] ]]$mean <- c(cdo.mean(ref.file,period,mask=mask), cdo.mean(ref.file,period,mask=mask,seasonal=T))
+  if(!is.null(reference)){
+    ref.file <- getReference(reference,variable)
+    store.name <- paste(reference,variable,sep=".")
+    store[[store.name]]$spatial.sd <- c(cdo.spatSd(ref.file,period), cdo.spatSd(ref.file,period,seasonal=T))
+    store[[store.name]]$mean <- c(cdo.mean(ref.file,period), cdo.mean(ref.file,period,seasonal=T))
+    
+    for(i in 1:length(srex.regions)){
+      getPolCoords(shape,i,destfile=mask)
+      store[[ store.name ]][[ srex.regions[i] ]]$spatial.sd <- c(cdo.spatSd(ref.file,period,mask=mask), cdo.spatSd(ref.file,period,mask=mask,seasonal=T))
+      store[[ store.name ]][[ srex.regions[i] ]]$mean <- c(cdo.mean(ref.file,period,mask=mask), cdo.mean(ref.file,period,mask=mask,seasonal=T))
+    } 
   }
   
   ngcm <- length(cmip5.urls(varid=variable))
@@ -78,19 +88,18 @@ calculate.statistics.cmip <- function(reference="era", period=c(1981,2010), vari
     store.name <- paste("gcm",i,sep=".")
     store[[store.name]]$spatial.sd <- c(cdo.spatSd(gcm.file,period),cdo.spatSd(gcm.file,period,seasonal=T))
     store[[store.name]]$mean <- c(cdo.mean(gcm.file,period),cdo.mean(gcm.file,period,seasonal=T))
-    store[[store.name]]$corr <- c(cdo.gridcor(gcm.file,ref.file,period),cdo.gridcor(gcm.file,ref.file,period,seasonal=T))
+    if(!is.null(reference))store[[store.name]]$corr <- c(cdo.gridcor(gcm.file,ref.file,period),cdo.gridcor(gcm.file,ref.file,period,seasonal=T))
     for(j in 1:length(srex.regions)){
-      getPolCoords(shape,i,destfile=mask)
+      getPolCoords(shape,j,destfile=mask)
       store[[store.name]][[srex.regions[j]]]$spatial.sd <- c(cdo.spatSd(gcm.file,period,mask=mask), cdo.spatSd(gcm.file,period,mask=mask,seasonal=T))
       store[[store.name]][[srex.regions[j]]]$mean <- c(cdo.mean(gcm.file,period,mask=mask), cdo.mean(gcm.file,period,mask=mask,seasonal=T))
-      store[[store.name]][[srex.regions[j]]]$corr <- c(cdo.gridcor(gcm.file,ref.file,period,mask=mask), cdo.gridcor(gcm.file,ref.file,period,mask=mask,seasonal=T))
+      if(!is.null(reference))store[[store.name]][[srex.regions[j]]]$corr <- c(cdo.gridcor(gcm.file,ref.file,period,mask=mask), cdo.gridcor(gcm.file,ref.file,period,mask=mask,seasonal=T))
     }
     saveRDS(store,store.file)
-    gc()
-    if(i==ngcm)return(invisible(store))
+#    if(i==ngcm)return(invisible(store))#Redundant check
   }
   return(invisible(store))
 }
 
-store <- calculate.statistics.cmip(opt$reference,c(opt$period1,opt$period2),opt$variable,opt$nfiles,
+store <- calculate.statistics.cmip(NULL,c(opt$period1,opt$period2),opt$variable,opt$nfiles,
                           opt$continue,opt$verbose,opt$mask)
