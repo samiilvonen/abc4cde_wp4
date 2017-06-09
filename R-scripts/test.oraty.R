9f73eaeb86b2639c8b3b02cc3245d2f6fc0d6940
kajsamp <kajsamp@met.no>
2017-06-09 12:29:41 +0200

    minor updates front-end
    Former-commit-id: 57b286cd130919479b12bb3a8ffe4e55e4c4ec4f

--- R-scripts/test.oraty.R
+++ R-scripts/test.oraty.R
@@ -1,5 +1,6 @@
 ## test.ratyo.R
 ## Test Olle's back-end functions
+library(DECM)
 
 file.meta <- find.file("metaextract.rda")
 file.meta <- file.meta[grep("abc4cde",file.meta)]
@@ -13,3 +14,26 @@ X <- get.shapefile(file.shape,with.path=TRUE)
 #python.getEra(1979,2016,"tas",0,type,stream,outfile)
 
 getCFSR()
+
+verbose <- TRUE
+X <- getGCMs(select=1:9,varid="tas",verbose=verbose)
+Y <- getGCMs(select=1:9,varid="pr",verbose=verbose)
+X.all$mean <- lapply(X,function(x) cdo.mean(x,period=c(1850,2100)))
+X.all$spatSd <- lapply(X,function(x) cdo.spatSd(x,period=c(1850,2100)))
+X.present$mean <- lapply(X,function(x) cdo.mean(x,period=c(1971,2000)))
+X.present$spatSd <- lapply(X,function(x) cdo.spatSd(x,period=c(1971,2000)))
+#X.present$timeSd <- lapply(X,function(x) cdo.timeSd(x,period=c(1971,2000)))
+X.near$mean <- lapply(X,function(x) cdo.mean(x,period=c(2021,2050)))
+X.near$spatSd <- lapply(X,function(x) cdo.spatSd(x,period=c(2021,2050)))
+X.far$mean <- lapply(X,function(x) cdo.mean(x,period=c(2071,2100)))
+X.far$spatSd <- lapply(X,function(x) cdo.spatSd(x,period=c(2071,2100)))
+
+Y.all$mean <- lapply(Y,function(x) cdo.mean(x,period=c(1850,2100)))
+Y.all$spatSd <- lapply(Y,function(x) cdo.spatSd(x,period=c(1850,2100)))
+Y.present$mean <- lapply(Y,function(x) cdo.mean(x,period=c(1971,2000)))
+Y.present$spatSd <- lapply(Y,function(x) cdo.spatSd(x,period=c(1971,2000)))
+#Y.present$timeSd <- lapply(Y,function(x) cdo.timeSd(x,period=c(1971,2000)))
+Y.near$mean <- lapply(Y,function(x) cdo.mean(x,period=c(2021,2050)))
+Y.near$spatSd <- lapply(Y,function(x) cdo.spatSd(x,period=c(2021,2050)))
+Y.far$mean <- lapply(Y,function(x) cdo.mean(x,period=c(2071,2100)))
+Y.far$spatSd <- lapply(Y,function(x) cdo.spatSd(x,period=c(2071,2100)))
