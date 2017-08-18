43579aac04b89d22631161cd07a6e9a4edc29685
kajsamp <kajsamp@met.no>
2017-08-18 13:54:04 +0200

    updated app dpdt
    Former-commit-id: b9e31faf6d2dd6f3a161845affa41e9e779c09ac

--- front-end/dpdt/server.R
+++ front-end/dpdt/server.R
@@ -4,7 +4,7 @@ library(DECM)
 library(DT)
 source("helpers.R")
 
-## load statistics
+## Load statistics calculated with script 'calculate_statistics.R'
 stats <- NULL
 data("statistics.cmip.era.tas.1981-2010")
 stats$tas$present <- store
@@ -19,17 +19,13 @@ stats$pr$nf <- store
 data("statistics.cmip.pr.2071-2100")
 stats$pr$ff <- store
 
-dT <- list()
-dPr <- list()
-gcms <- names(stats$tas$ff)
-for (gcm in gcms) {
-  dT[["nf"]][[gcm]] <- stats$tas$nf[[gcm]]$mean - stats$tas$present[[gcm]]$mean
-  dT[["ff"]][[gcm]] <- stats$tas$ff[[gcm]]$mean - stats$tas$present[[gcm]]$mean
-  dPr[["nf"]][[gcm]] <- stats$pr$nf[[gcm]]$mean - stats$pr$present[[gcm]]$mean
-  dPr[["ff"]][[gcm]] <- stats$pr$ff[[gcm]]$mean - stats$pr$present[[gcm]]$mean
-}
+## Function 'regions' is defined in helpers.R
+srex <- regions("srex")
 
-# Define a server for the Shiny app
+## Load geographical data for map
+data("geoborders",envir=environment())
+  
+## Define a server for the Shiny app
 shinyServer(function(input, output) {
   
   output$dtdpr <- renderPlot({
@@ -39,15 +35,49 @@ shinyServer(function(input, output) {
     period <- switch(tolower(as.character(input$period)),
                      "far future (2071-2100)"='ff',
                      "near future (2021-2050)"='nf')
-    dtas <- unlist(lapply(dT[[period]], function(x) x[[season]]))
-    dpr <- unlist(lapply(dPr[[period]], function(x) x[[season]]))
+    gcms <- names(stats$tas$ff)
+    if(tolower(input$region)=="global") {
+      coord <- list(lon=c(-180,180),lat=c(-90,90))
+      dtas <- sapply(gcms, function(gcm) stats$tas[[period]][[gcm]][["mean"]][[season]] - 
+                       stats$tas$present[[gcm]][["mean"]][[season]]) 
+      dpr <- sapply(gcms, function(gcm) stats$pr[[period]][[gcm]][["mean"]][[season]] - 
+                      stats$pr$present[[gcm]][["mean"]][[season]])
+    } else {
+      i.srex <- which(srex$name==input$region)
+      region <- srex$label[i.srex]
+      dtas <- sapply(gcms, function(gcm) stats$tas[[period]][[gcm]][[region]][["mean"]][[season]] - 
+                                         stats$tas$present[[gcm]][[region]][["mean"]][[season]]) 
+      dpr <- sapply(gcms, function(gcm) stats$pr[[period]][[gcm]][[region]][["mean"]][[season]] - 
+                                        stats$pr$present[[gcm]][[region]][["mean"]][[season]])
+    }
     im <- as.numeric(gsub(":.*","",input$gcms))
-    scatterplot(dtas,dpr*(60*60*24),ix=NULL,xlim=c(-3,3),ylim=c(-0.2,0.2),
-                xlab="Temperature (deg C)",ylab="Precipitation (mm/day)",
-                main=paste("Climate change assuming RCP4.5\npresent day to",input$period),
+    scatterplot(dtas,dpr*(60*60*24),ix=NULL,xlim=input$tlim,ylim=input$plim,
+                xlab="Temperature change (deg C)",ylab="Precipitation change (mm/day)",
+                main=paste("Climate change assuming RCP4.5\npresent day (1981-2010) to",input$period),
                 show.legend=FALSE,im=im,
                 legend=seq(length(dtas)),pal=NULL,#pal="cat",pch=21,
                 pch=as.character(seq(length(dtas))),cex=1.5,lwd=1.5,new=FALSE)
-  }, width=600, height=600)
+  }, width=function(){450}, height=function(){450})
+  
+  output$map <- renderPlot({
+    if(tolower(input$region)=="global") {
+      region <- list(lon=c(-180,-180,180,180,-180),lat=c(-90,90,90,-90,-90))
+    } else {
+      i.srex <- which(srex$name==input$region)
+      region <- list(lon=srex$coord[[i.srex]][1,],
+                     lat=srex$coord[[i.srex]][2,])
+    }
+    par(mgp=c(1,0.5,0),mar=c(0.2,0.2,0.2,0.2))
+    plot(geoborders$x,geoborders$y,col="grey30",type="l",lwd=0.5,
+         xlim=c(-180,180),ylim=c(-90,90),
+         xlab="Longitude",ylab="Latitude",xaxt="n",yaxt="n")
+    #lines(attr(geoborders,'borders')$x,attr(geoborders,'borders')$y,col="grey30")
+    par(xaxt="s",yaxt="s",las=1,col.axis='grey',col.lab='grey20',
+        cex.lab=0.7,cex.axis=0.7)
+    axis(3,at=pretty(par("xaxp")[1:2],n=5),col='grey50')
+    axis(2,at=pretty(par("yaxp")[1:2],n=5),col='grey50')
+    grid()
+    lines(region$lon,region$lat,col="blue",lwd=1.5,lty=1)
+  }, width=function(){200},height=function(){200}*0.6)#width=250, height=175)
   
 })
